9222f72c5000ffe587ca9d2a385dc86c6061c3ef
Muhammad Hanzla Tariq <hanzlasial210@gmail.com>
2026-03-29 13:05:06 +0500

    Add Clear Function

---
+++ calculator.py
@@ -0,0 +1,57 @@
+def add (sum_num1, sum_num2):
+    res = sum_num1 + sum_num2
+    return res
+def sub (sub_num1, sub_num2):
+    res = sub_num1 - sub_num2
+    return res
+def mul (num1, num2):
+    res = num1 * num2
+    return res
+def div (num1 , num2):
+    res = num1 / num2
+    return res
+def power (num1, num2):
+    res = num1 ** num2
+    return res
+def main() :
+    oper1 = int(input("Enter first number: "))
+    cont = True
+    while cont:
+        op = input("Enter operator(+, -, *, /, ^):")
+        oper2 = int(input("Enter second number: "))
+        if op == "+" :
+           Result = add(oper1, oper2)
+           print("Result:", Result )
+        elif op == "-" :
+           Result = sub(oper1, oper2)
+           print("Result: ", Result)
+        elif op == "*" :
+           Result = mul(oper1, oper2)
+           print("Result:", Result)
+        elif op == "/" :
+           if oper2 == 0:
+               print("Error: Can't divide by zero:")
+               return
+           Result = div(oper1, oper2)
+           print("Result:", Result)
+        elif op == "^":
+            Result = power(oper1, oper2)
+            print("Result:", Result)
+        else:
+            print("Invalid operator")
+            continue
+        res= input("Do you want to continue(y,n) or clear(c): ").lower()
+        if res=="y":
+          oper1 = Result
+        elif res=="c":
+           Result = 0
+           print("Result cleared successfully")
+           oper1=int(input("Enter first number: "))
+           continue
+        elif res=="n":
+          print("Final results:", Result)
+          cont= False
+        else:
+          print("Enter valid character (y,n)")
+if __name__ == "__main__":
+   main()
