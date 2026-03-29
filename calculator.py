0a12c0194f0111438ad3fee212510b20091b7853
Muhammad Hanzla Tariq <hanzlasial210@gmail.com>
2026-03-29 13:31:13 +0500

    Remove Mistakes

--- calculator.py
+++ calculator.py
@@ -14,11 +14,11 @@ def power (num1, num2):
     res = num1 ** num2
     return res
 def main() :
-    oper1 = int(input("Enter first number: "))
+    oper1 = float(input("Enter first number: "))
     cont = True
     while cont:
         op = input("Enter operator(+, -, *, /, ^):")
-        oper2 = int(input("Enter second number: "))
+        oper2 = float(input("Enter second number: "))
         if op == "+" :
            Result = add(oper1, oper2)
            print("Result:", Result )
@@ -31,7 +31,7 @@ def main() :
         elif op == "/" :
            if oper2 == 0:
                print("Error: Can't divide by zero:")
-               return
+               continue
            Result = div(oper1, oper2)
            print("Result:", Result)
         elif op == "^":
@@ -40,18 +40,21 @@ def main() :
         else:
             print("Invalid operator")
             continue
-        res= input("Do you want to continue(y,n) or clear(c): ").lower()
-        if res=="y":
-          oper1 = Result
-        elif res=="c":
-           Result = 0
-           print("Result cleared successfully")
-           oper1=int(input("Enter first number: "))
-           continue
-        elif res=="n":
-          print("Final results:", Result)
-          cont= False
-        else:
-          print("Enter valid character (y,n)")
+        while True:
+            res = input("Do you want to continue(y,n) or clear(c): ").lower()
+            if res == "y":
+                oper1 = Result
+                break
+            elif res == "c":
+                Result = 0
+                print("Result cleared successfully")
+                oper1 = float(input("Enter first number: "))
+                break
+            elif res == "n":
+                print("Final results:", Result)
+                cont = False
+                break
+            else:
+                print("Enter valid character (y,n,c)")
 if __name__ == "__main__":
    main()
